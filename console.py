86feeaae3811eb28387ebf152b4033bf8a715b7a
David <obododavidsop@gmail.com>
2024-01-15 21:01:38 +0100

    do create updated but not working

--- console.py
+++ console.py
@@ -73,8 +73,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
+                    if pline[0] == '{' and pline[-1] == '}' \
+                    and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -113,18 +113,49 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    def do_create(self, argv):
         """ Create an object of any class"""
-        if not args:
+        args = argv.split()
+        if not args[0]:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        # create Place city_id="0001" user_id="0001" 
+        # name="My_little_house" number_rooms=4 
+        # number_bathrooms=2 max_guest=10 
+        # price_by_night=300 latitude=37.773972 longitude=-122.431297
+
+        # Create a dictionary
+        attr = {}
+
+        # Loop through params and check condition.
+        for i in range(1, len(args)):
+            param = args[i]
+            # param => city_id="0001"
+
+            param_splitted = param.split("=")
+            # param_splitted = (city_id, "0001")
+
+            # check conditions
+            # {city_id: 0001, user_id: 0001, name: "My little house"}
+            if isinstance(param_splitted[1], str):
+            # check1: String
+                param_splitted[1] = param_splitted[1].replace('"', '')
+                param_splitted[1] = param_splitted[1].replace('_', ' ')
+                attr[param_splitted[0]] = param_splitted[1]
+                # value = 0001
+            # check3: Integer (Default case)
+            # check2: Float
+            elif isinstance(param_splitted[1], float) or isinstance(param_splitted[1], int):
+                attr[param_splitted[0]] = param_splitted[1]
+
+        # Pass attr into the new instance
+        # print(attr)
+        new_instance = HBNBCommand.classes[args[0]](**attr)
+        # print(new_instance.id)
+        new_instance.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +303,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +311,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
