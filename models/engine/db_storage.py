6e7a99dc350c2d0f5f74969bb85997d2d9d3ac70
David <obododavidsop@gmail.com>
2024-01-17 23:16:29 +0100

    Update env

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,19 +17,20 @@ classes = {
             'Review': Review
 }
 
+# Environment variables
+HBNB_ENV = getenv('HBNB_ENV')
+HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+
 
 class DBStorage():
     """ Represents a DBStorage class """
     __engine = None
     __session = None
 
-    # Environment variables
-    HBNB_ENV = getenv('HBNB_ENV')
-    HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-    HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-    HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-    HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-    HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 
     def __init__(self):
