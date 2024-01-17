7c9666aa8990e158b85dd1190dd8e73c99216ef4
David <obododavidsop@gmail.com>
2024-01-18 00:00:50 +0100

    updated do_all in console.py

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ Console Module """
 import cmd
 import sys
-from models.base_model import BaseModel
+from models.base_model import BaseModel, storage_type
 from models.__init__ import storage
 from models.user import User
 from models.place import Place
@@ -229,36 +229,22 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
-    def do_all(self, arg):
+    def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-        # print_list = []
+        new_list = []
 
-        # if args:
-        #     args = args.split(' ')[0]  # remove possible trailing args
-        #     if args not in HBNBCommand.classes:
-        #         print("** class doesn't exist **")
-        #         return
-        #     for k, v in storage._FileStorage__objects.items():
-        #         if k.split('.')[0] == args:
-        #             print_list.append(str(v))
-        # else:
-        #     for k, v in storage._FileStorage__objects.items():
-        #         print_list.append(str(v))
-
-        # print(print_list)
-        my_ls = []
-        all_files = storage.all()
-        if len(arg) == 0:
-            for fs in all_files.values():
-                my_ls.append(fs.__str__())
-            print("{}".format(my_ls))
-        elif arg not in self.models.keys():
-            print("** class doesn't exist **")
+        if args:
+            args = args.split(' ')[0]  # remove possible trailing args
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for k, v in storage.all(args).items():
+                new_list.append(str(v))
         else:
-            for v in all_files.values():
-                if v.__class__.__name__ == arg:
-                    my_ls.append(v.__str__())
-            print("{}".format(my_ls))
+            for k, v in storage.all().items():
+                new_list.append(str(v))
+
+        print(new_list)
 
     def help_all(self):
         """ Help information for the all command """
