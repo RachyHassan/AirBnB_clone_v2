e8a788709c4463be1f151d9a183ac66642dcb8ac
David <obododavidsop@gmail.com>
2024-02-02 02:00:09 +0100

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Compress before sending"""
+
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """Packs the web_static folder"""
-    # web_static_<year><month><day><hour><minute><second>.tgz
+    """Packs the web_static folder into a tgz file"""
+
     today_date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "web_static_" + today_date + ".tgz"
+    filename = "versions/web_static_" + today_date + ".tgz"
     try:
         local('mkdir -p versions/')
-        local("tar -zcvf {} versions/".format(filename))
+        local("tar -zcvf {} web_static/".format(filename))
+        return filename
     except Exception:
         return None
