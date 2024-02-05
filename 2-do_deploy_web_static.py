3d221cd04bae77f07df122f3a6d5a69774e231e5
David <obododavidsop@gmail.com>
2024-02-05 14:49:30 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
 """Compress before sending"""
-
 from fabric.api import put, run, env
 import os
 
 
-env.hosts = ['54.236.47.245', '100.26.156.253']
-
-
+# Prototype: def do_deploy(archive_path):
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
-
+    # Returns False if the file at the path archive_path doesn’t exist
     if not os.path.exists(archive_path):
         return False
+    # (self) archive path versions/web_static_1983929.tgz
     try:
+        # The script should take the following steps:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+        # on the web server
         archive = archive_path.split('/')[-1]
-        put(archive_path, f'/tmp/{archive}')
-        archive_folder = archive.split('.')
-        path = "/data/web_static/releases"
-        run("mkdir -p {}/{}/".format(path, archive_folder[0]))
-        new_archive = '.'.join(archive_folder)
-        run("tar -xzf /tmp/{} -C {}/{}/"
-            .format(new_archive, path, archive_folder[0]))
-        run("rm /tmp/{}".format(archive))
-        run("mv {}/{}/web_static/* {}/{}/"
-            .format(path, archive_folder[0], path, archive_folder[0]))
-        run("rm -rf {}/{}/web_static".format(path, archive_folder[0]))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf {}/{} /data/web_static/current"
-            .format(path, archive_folder[0]))
+        archive_no_ext = archive.split('.')[0]
+        run(f"mkdir -p /data/web_static/releases/{archive_no_ext}/")
+        run(f"tar -zxf /tmp/{archive} -C \
+            /data/web_static/releases/{archive_no_ext}/")
+        # Delete the archive from the web server
+        run(f"rm -f /tmp/{archive}")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm /data/web_static/current")
+        # Create a new the symbolic link /data/web_static/current on the
+        # web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension>)
+        run(f"ln -sf /data/web_static/releases/{archive_no_ext}/ \
+            /data/web_static/current")
         return True
     except Exception:
         return False
+    # Returns True if all operations have been done correctly,
+    # otherwise returns False
+
+# All remote commands must be executed on your both web servers
+# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+env.hosts = ['54.236.47.245', '100.26.156.253']
---
+++ exec.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+fab -f '2-do_deploy_web_static.py' do_deploy:archive_path=versions/web_static/web_static_20240205144518.tgz -i ~/.ssh/school -u ubuntu
