2cef4ea4eb67893f1f620f32d941d623d441ca03
David <obododavidsop@gmail.com>
2024-02-09 04:59:09 +0100

    updated 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Compress before sending"""
-from fabric.api import local
-import os
+"""Clean up my web static"""
+from fabric.api import local, env, run
 
 
 # (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
-# env.hosts = ['54.236.47.245', '100.26.156.253']
-# env.user = "ubuntu"
+env.hosts = ['54.236.47.245', '100.26.156.253']
+env.user = "ubuntu"
+
 
 def do_clean(number=0):
     """ Deletes out-of-date archives """
     number = int(number)
     if number in [0, 1]:
-        number == 1
-    local('cd versions')
-    total = len(os.listdir('.'))
-    # local('ls -t | tail -n {} | xargs rm'.format(total - number))
-    local('echo "Total: {}\nNumber: +{}" > file.txt'.format(total, number))
+        number = 2
+    else:
+        number =+ 1
+    # tail -n +{NUM} => use  -n +NUM to output starting with line NUM
+    local(f'cd ./versions; ls -t | tail -n +{number} | xargs rm -rf')
+    folder = '/data/web_static/releases'
+    run(f'cd {folder}; ls -t | tail -n +{number} | xargs rm -rf')
