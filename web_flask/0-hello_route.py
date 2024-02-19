26207e4859963ed37b497a30f41871c5583f417d
David <obododavidsop@gmail.com>
2024-02-19 21:01:30 +0100

    Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+"""Hello flask"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Prints Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
