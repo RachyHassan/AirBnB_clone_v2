fc4b1d9259377bb3a6dc85e6cd2324f982d87152
David <obododavidsop@gmail.com>
2024-02-19 21:10:22 +0100

    updated 0 and 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Hello flask"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Prints Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """Prints HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
