197006b7fdbf26c4e2b471cd6de26f3d417febd1
RachyHassan <tolulopehassan97@gmail.com>
2024-02-22 00:37:54 +0100

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-"""Hello flask"""
+"""A script that starts a Flask web application
+Requirements:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ”, followed by the value
+        of the text variable (replace underscore _ symbols with a space )
+        /python/(<text>): display “Python ”, followed by the value of
+        the text variable (replace underscore _ symbols with a space )
+        The default value of text is “is cool”
+        /number/<n>: display “n is a number” only if n is an integer
+        /number_template/<n>: display a HTML page only if n is an integer:
+            H1 tag: “Number: n” inside the tag BODY
+"""
 from flask import Flask, render_template
 
 
@@ -9,38 +23,38 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello():
-    """Prints Hello HBNB!"""
+    """A function that prints Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Prints HBNB"""
+    """A function that prints HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>')
-def cisfun(text):
-    """C is fun"""
+def c_is_fun(text):
+    """A function that returns C is fun"""
     return 'C ' + text.replace("_", " ")
 
 
 @app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
 def python_is_fun(text='is_cool'):
-    """Python is cool"""
+    """A function that returns Python is cool"""
     return 'Python ' + text.replace("_", " ")
 
 
 @app.route('/number/<int:n>')
-def is_n_number(n):
-    """Is n a number"""
+def number_n(n):
+    """A function that prints "n" Is a number"""
     return f'{n} is a number'
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """Render template if n is a number"""
+    """A function that displays an HTML page if n is a number"""
     return render_template('5-number.html', n=n)
 
 
